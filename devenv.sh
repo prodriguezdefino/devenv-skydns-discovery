ceb76bf4e0830fe43a4621bae3a277c5e6ed80b6
pablo rodriguez defino <prodriguezdefino@gmail.com>
2016-10-14 16:46:16 -0700

    inclusion of the run, remove, stop scripts

--- devenv.sh
+++ devenv.sh
@@ -13,19 +13,33 @@ case "$1" in
         source internal/devenv-reload-containers.sh
         ;;
     logs)
-        source internal/devenv-logs.sh $2
+        source internal/devenv-logs.sh "$2"
         ;;
     status)
-        source internal/devenv-status.sh $2
+        source internal/devenv-status.sh "$2"
         ;;
     up)
         source internal/devenv-up.sh
         ;;
     inspect)
-        source internal/devenv-inspect.sh $2
+        source internal/devenv-inspect.sh "$2"
+        ;;
+    stop)
+        source internal/devenv-stop.sh "$2"
+        ;;
+    remove)
+        source internal/devenv-remove.sh "$2"
+        ;;
+    run)
+        runparam=""
+        for var in ${@:2}
+        do
+            runparam="$runparam $var"
+        done
+        source internal/devenv-run.sh "$runparam" 
         ;;
      
     *)
-        echo $"Usage: $0 {down|reload|logs <container>|up|inspect <container>|status <optional-flags>}"
+        echo $"Usage: $0 {down|reload|logs <container>|up|inspect <container>|status <-a>|run <parameters>|remove <container>|stop <container>}"
         exit 1
 esac
